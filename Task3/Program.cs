50ead79fa3e798c6a752f57e0bae8b15d2cb8677
«Sheluto» <sheluto@gmail.com>
2022-10-19 16:43:19 +0300

    Решила задачу 3

---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти: ");
+int number = int.Parse(Console.ReadLine()??"0");
+if (number==1)
+    {
+        Console.WriteLine("X: 0, +бесконечность");
+        Console.WriteLine("Y: 0, +бесконечность");
+    }
+if (number==2)
+    {
+        Console.WriteLine("X: 0, -бесконечность");
+        Console.WriteLine("Y: 0, +бесконечность");
+    }
+if (number==3)
+    {
+        Console.WriteLine("X: 0, -бесконечность");
+        Console.WriteLine("Y: 0, -бесконечность");
+    }
+if (number==4)
+    {
+        Console.WriteLine("X: 0, +бесконечность");
+        Console.WriteLine("Y: 0, -бесконечность");
+    }
+if (number>4)
+{
+    Console.WriteLine("Такой четверти не существует. Введите число от 1 до 4.");
+}
+if (number<1)
+{
+    Console.WriteLine("Такой четверти не существует. Введите число от 1 до 4.");
+}
